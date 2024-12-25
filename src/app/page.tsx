e4895a5b6256102a2d0b27b91b13670f7dd1e8f3
ajurian <adolfjamesurian@gmail.com>
2024-12-25 21:58:05 +0800

    Fixed layout bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Grades from "@/components/Home/Grades";
 
 export default function Home() {
     return (
-        <div className="relative flex min-h-screen flex-wrap justify-center font-sans xl:flex-nowrap">
+        <div className="relative flex min-h-screen flex-col items-center justify-center font-sans xl:flex-row">
             <GradeCalculator />
             <Grades />
         </div>
