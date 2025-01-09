5e4478db22af18e092116c8a4ba2684a2f8bb5d6
ajurian <adolfjamesurian@gmail.com>
2025-01-09 17:27:21 +0800

    Set `step=any` to FormField input

--- src/components/Home/GradeCalculator/GradeCalculatorForm/FormField.tsx
+++ src/components/Home/GradeCalculator/GradeCalculatorForm/FormField.tsx
@@ -31,6 +31,7 @@ export default function FormField({
                 value={value}
                 onChange={handleInputChange}
                 onFocus={handleInputFocus}
+                step="any"
             />
         </div>
     );
